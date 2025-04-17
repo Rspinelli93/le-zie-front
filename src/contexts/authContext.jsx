1f68b231a36ff2bfa597d68e20df70d733840569
RiccardoS <rspinellir13@gmail.com>
2025-04-17 08:44:52 +0200

    Auth correction

--- src/contexts/authContext.jsx
+++ src/contexts/authContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { getValidAdminToken } from '../authentication/adminAuth';
+import { getValidAdminToken } from '../authentication/AdminAuth';
 
 const AuthContext = createContext();
 
